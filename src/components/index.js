4fcfb70934b1aa255c2d0fbb936b08be71aa56cc
Abiola Olamilekan <abiolawalter2015@gmail.com>
2024-01-04 11:42:53 +0100

    feat: implement folder structure

---
+++ src/components/index.js
@@ -0,0 +1,23 @@
+import Billing from "./Billing";
+import Business from "./Business";
+import CardDeal from "./CardDeal";
+import Clients from "./Clients";
+import Cta from "./CallToAction";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import Stats from "./Stats";
+import Testimonials from "./Testimonials";
+
+export {
+  Billing,
+  Business,
+  CardDeal,
+  Clients,
+  Cta,
+  Footer,
+  Hero,
+  Navbar,
+  Stats,
+  Testimonials,
+};
